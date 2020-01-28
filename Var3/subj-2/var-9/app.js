773ca00e88fab55497ba1bfd221a69347d2b463b
Ubuntu <ubuntu@ip-172-31-1-108.us-east-2.compute.internal>
2020-01-28 21:25:00 +0000

    subj2

--- Var3/subj-2/var-9/app.js
+++ Var3/subj-2/var-9/app.js
@@ -25,6 +25,32 @@ app.get('/cars', (req, res) => {
 });
 
 app.post('/cars', (req, res, next) => {
+    
+    if(Object.keys(req.body).length!==0){
+        if(req.body.hasOwnProperty('make') || req.body.hasOwnProperty('model') || req.body.hasOwnProperty('price')){
+            if(req.body.price>0){
+                for(var i=0;i<app.locals.cars.length;i++){
+                    if(app.locals.cars[i].model === req.body.model){
+                    res.status(500).json({message:'Car already exists'})
+                    }
+                }
+                console.log(req.body)
+                app.locals.cars.push(req.body)
+                res.status(201).json({message: 'Created'})
+            }
+            else{
+                 res.status(500).json({message:'Price should be a positive number'})
+            }
+        }
+        else{
+            res.status(500).json({message : 'Invalid body format'})
+        }
+    }
+    else{
+         res.status(500).json({message: 'Body is missing'})
+    }
+    
+    
     res.status(400).json({message: 'Bad request'});
 })
 
