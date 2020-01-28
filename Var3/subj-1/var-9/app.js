b72c824f385f1cd745b61d0bb9f4a4cb263067fe
Ubuntu <ubuntu@ip-172-31-1-108.us-east-2.compute.internal>
2020-01-28 21:21:43 +0000

    m

--- Var3/subj-1/var-9/app.js
+++ Var3/subj-1/var-9/app.js
@@ -1,4 +1,31 @@
 function applyBlackFriday(products, discount){
+   
+     return new Promise((resolve,reject)=>{
+       if(typeof discount === 'number'){
+           if(discount>0 && discount<=10){
+               for(var i=0;i<products.length;i++){
+                     if(typeof products[i].price !== 'number' && typeof products[i].name !== 'string' ){
+                       reject(new Error('Invalid array format'))
+                      }
+                     else {
+                       discount = discount/100;
+                       products[i].price = products[i].price - products[i].price*discount;
+                       console.log(products[i].price)
+                     }
+                }
+           }
+           else{
+               reject(new Error('Discount not applicable'))
+           }
+       }
+       else{
+           reject(new Error('Invalid discount'))
+       }
+       
+         
+     })
+
+    
 }
 
 const app = {
